4d19edd31f7bbe28f72eb4902f04e9c6a216b249
Eduardo11290! <petru-eduard.stefoni@student.upt.ro>
2026-01-18 09:35:24 -0500

    lint files

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from .models import CarMake, CarModel
 
-# Optional: Registering CarModel with a custom ModelAdmin to make it easier to view
+
+# Registering CarModel with a custom ModelAdmin
 class CarModelAdmin(admin.ModelAdmin):
     list_display = ('name', 'car_make', 'type', 'year', 'dealer_id')
     list_filter = ('type', 'car_make', 'year')
     search_fields = ('name', 'car_make__name')
 
+
 # Registering models with their respective admins
 admin.site.register(CarMake)
 admin.site.register(CarModel, CarModelAdmin)
-
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,13 +1,13 @@
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 # Create your models here.
 
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    
+
     def __str__(self):
         return self.name  # Return the name as the string representation
 
@@ -24,9 +24,11 @@ class CarModel(models.Model):
     ]
 
     # Fields
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    # Many-to-One relationship
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
-    dealer_id = models.IntegerField()  # Refers to a dealer created in Cloudant database
+    # Refers to a dealer created in Cloudant database
+    dealer_id = models.IntegerField()
     type = models.CharField(max_length=10, choices=CAR_TYPES, default=SUV)
     year = models.IntegerField(
         default=2023,
@@ -37,4 +39,4 @@ class CarModel(models.Model):
     )
 
     def __str__(self):
-        return f"{self.car_make.name} {self.name}"  # Returns "Make Model" (e.g., Toyota Camry)
+        return f"{self.car_make.name} {self.name}"  # Return "Make Model"
