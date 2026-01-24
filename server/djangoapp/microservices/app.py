9bb06ad73f7421e72427dd2862d7116b0940b9ee
Eduardo11290 <petru-eduard.stefoni@student.upt.ro>
2026-01-24 20:21:04 +0200

    Improve welcome message format and add newline
    Refactor welcome message for better readability and add a newline at the end of the file.

--- server/djangoapp/microservices/app.py
+++ server/djangoapp/microservices/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from nltk.sentiment import SentimentIntensityAnalyzer
 import nltk  # Import nltk
-import json
+
 
 # Download the required lexicon
 nltk.download('vader_lexicon')
@@ -10,9 +10,12 @@ app = Flask("Sentiment Analyzer")
 
 sia = SentimentIntensityAnalyzer()
 
+
 @app.get('/')
 def home():
-    return "Welcome to the Sentiment Analyzer. Use /analyze/text to get the sentiment"
+    return ("Welcome to the Sentiment Analyzer. "
+            "Use /analyze/text to get the sentiment")
+
 
 @app.get('/analyze/<input_txt>')
 def analyze_sentiment(input_txt):
@@ -27,10 +30,12 @@ def analyze_sentiment(input_txt):
         res = "negative"
     elif (neu > neg and neu > pos):
         res = "neutral"
-    
+
     # It is better practice to return a proper JSON response object
     response_data = {"sentiment": res}
     return response_data
 
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
+    
